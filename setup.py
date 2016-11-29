Kitware/SMQTK-Image-IO

commit c07a770ae2c85b9f3ce70d87e106607799e5e4a0

Added optional use of file-magic module for DataFileElement
If importable, try to use file-magic mime-type identification from file
bytes first. This should be more performant that use of tika and is more
robust than just checking file name extension string.

setup.py
@@ -154,6 +154,9 @@ setuptools.setup(
         'flann': [
             'pyflann>=1.8.4',
         ],
+        'libmagic', [
+            'file-magic',
+        ],
         'postgres': [
             'psycopg2',
         ],

commit 38e9d2dd74a05f8c0f1cc4d53368899005c6e79d

Fix syntax error in setup.py introduced by c07a770

setup.py
@@ -154,7 +154,7 @@ setuptools.setup(
         'flann': [
             'pyflann>=1.8.4',
         ],
-        'libmagic', [
+        'libmagic': [
             'file-magic',
         ],
         'postgres': [